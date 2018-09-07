7ca1bc034c40b03a189fa6305960ff1426ab33d5
Martin Daniel <mtin722@gmail.com>
2018-09-07 11:41:40 +1000

    Lineted events.js

--- js/events.js
+++ js/events.js
@@ -1,11 +1,11 @@
 function addEvents(e) {
-    for (let i = 0; i < e.length; i++) {
-        e[i].addEventListener("click", function () {
-            if (this.classList.contains("selected")) {
-                this.classList.remove("selected");
-            } else {
-                this.classList.add("selected");
-            };
-        });
-    };
+  for (let i = 0; i < e.length; i++) {
+    e[i].addEventListener('click', function () {
+      if (this.classList.contains('selected')) {
+        this.classList.remove('selected');
+      } else {
+        this.classList.add('selected');
+      }
+    });
+  }
 }
