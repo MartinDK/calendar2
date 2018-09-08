caea7fc19b33b3233788252ece3b6f5503482dbf
Martin Daniel <mtin722@gmail.com>
2018-09-08 23:10:31 +1000

    Transformed Calender2 into an ES6 Class

--- js/calendar2.js
+++ js/calendar2.js
@@ -1,60 +1,35 @@
 // Create a new calendar
 
-function Calendar2() {
-  // labels for the days of the week
-  this.daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+class Calendar2 {
+  constructor() {
+    // labels for the days of the week
+    this.daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-  // months array in order
-  this.monthsName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    // months array in order
+    this.monthsName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-  // the days of the week for each month, in order
-  this.daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-}
+    // the days of the week for each month, in order
+    this.daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-function createCalendar() {
-  const calendar2 = new Calendar2();
-  const monthHTML2 = calendar2.createMonthHTML();
-  document.getElementById('calendar2').insertAdjacentHTML('beforeend', monthHTML2);
-  return calendar2;
-}
+    this.createMonthArray = function() {
+      const monthLength = this.getToday().monthLength;
+      let monthArray = Array.apply(null, { length: monthLength }).map(Number.call, Number);
+      // Adjust first day 0=>1
+      monthArray = monthArray.map(x => x + 1);
 
-function today() {
-  return function () {
-    const today = new Date();
-    const date = today.getDate();
-    const day = this.daysOfWeek[today.getDay()];
-    const ordinal = this.dateOrdinal(date);
-    const monthLength = this.daysInMonth[today.getMonth()];
-    const month = this.monthsName[today.getMonth()];
-    const year = today.getFullYear();
-    const firstOfMonth = new Date(year, today.getMonth(), 1);
-    const firstDay = firstOfMonth.getDay() - 1;
-    return {
-      date,
-      day,
-      ordinal,
-      firstDay,
-      monthLength,
-      month,
-      year,
-    };
-  };
-}
+      return monthArray;
+    }
+  }
+  static createCalendar() { 
 
-function monthArray() {
-  return function () {
-    const monthLength = this.getToday().monthLength;
-    let monthArray = Array(...{
-      length: monthLength,
-    }).map(Number.call, Number);
-    // Adjust first day 0=>1
-    monthArray = monthArray.map(x => x + 1);
-    return monthArray;
-  };
-}
+    const calendar2 = new Calendar2();
+    const monthHTML2 = calendar2.createMonthHTML();
+    document.getElementById('calendar2').insertAdjacentHTML('beforeend', monthHTML2);
+    calendar2.highlightToday();
 
-function monthHTML() {
-  return function () {
+    return calendar2;
+  }
+  createMonthHTML() {
     const month = this.createMonthArray();
     const startWeekday = this.getToday().firstDay;
     let html = `<table class="calendar2 calendar-table"> <tbody ><tr><th colspan="10" > ${this.getToday().day} &nbsp;${this.getToday().date}<sup>th</sup>&nbsp;${this.getToday().month}&nbsp;${this.getToday().year}</th ></tr>`;
@@ -75,47 +50,60 @@ function monthHTML() {
       }
     }
     html += '</table>';
-    return html;
-  };
-}
 
-function highlight() {
-  return function () {
+    return html;
+  }
+  highlightToday() {
     const e = document.querySelectorAll(`#id-${this.getToday().date}`);
     e[0].classList.add('today');
-  };
-}
-
-function ordinal() {
-  return function (date) {
-    // number ordinal labels
-    const numberOrdinal = {
-      0: 'th',
-      1: 'st',
-      2: 'nd',
-      3: 'rd',
-      4: 'th',
-      5: 'th',
-      6: 'th',
-      7: 'th',
-      8: 'th',
-      9: 'th',
+  }
+  dateOrdinal() {
+    return function (date) {
+      const numberOrdinal = {
+        0: 'th',
+        1: 'st',
+        2: 'nd',
+        3: 'rd',
+        4: 'th',
+        5: 'th',
+        6: 'th',
+        7: 'th',
+        8: 'th',
+        9: 'th',
+      };
+      let i;
+      if (date >= 30) {
+        i = date - 30;
+      } else if (date >= 20) {
+        i = date - 20;
+      } else if (date >= 10) {
+        i = date - 10;
+      } else {
+        i = date;
+      }
+      
+      return numberOrdinal[i];
     };
-    let i;
-    if (date >= 30) {
-      i = date - 30;
-    } else if (date >= 20) {
-      i = date - 20;
-    } else if (date >= 10) {
-      i = date - 10;
-    } else {
-      i = date;
-    }
-    return numberOrdinal[i];
+  }
+  getToday() {
+    const today = new Date();
+    const date = today.getDate();
+    const day = this.daysOfWeek[today.getDay()];
+    const ordinal = this.dateOrdinal(date);
+    const monthLength = this.daysInMonth[today.getMonth()];
+    const month = this.monthsName[today.getMonth()];
+    const year = today.getFullYear();
+    const firstOfMonth = new Date(year, today.getMonth(), 1);
+    const firstDay = firstOfMonth.getDay() - 1;
+    
+    return {
+      date,
+      day,
+      ordinal,
+      firstDay,
+      monthLength,
+      month,
+      year,
+    };
   };
 }
-Calendar2.prototype.getToday = today();
-Calendar2.prototype.createMonthArray = monthArray();
-Calendar2.prototype.createMonthHTML = monthHTML();
-Calendar2.prototype.highlightToday = highlight();
-Calendar2.prototype.dateOrdinal = ordinal();
