7328bc453ca0f8dd40dca67056afca6696d77228
Martin Daniel <mtin722@gmail.com>
2018-09-09 15:14:16 +1000

    Fixed event handler

--- js/events.js
+++ js/events.js
@@ -1,18 +1,42 @@
+// Create Events
 function addEvents(selector) {
   const el = document.querySelectorAll(selector);
 
   for (let i = 0; i < el.length; i += 1) {
-    el[i].addEventListener('click', eventHandler(this));
+    el[i].addEventListener('click', () => {
+      eventHandler(el[i])
+    });
   };
 }
 
 // Event handler
-function eventHandler(el) {
-console.log(el);
+let eventHandler = (el) => {
 
+  if (el.classList.contains('today')) { return selectToday(el);
+  
+  }
   if (el.classList.contains('selected')) {
-    el.classList.remove('selected');
+
+    return el.classList.remove('selected');
+
   } else {
-    el.classList.add('selected');
+
+    return el.classList.add('selected');
+
   };
+}
+
+// Events
+let toggleSelection = el => {
+
+}
+let selectToday = el => { 
+  if (el.classList.contains('selected')) {
+    console.log(`goodbye`);
+    return el.classList.remove('selected')
+  } else {
+    console.log(`hello`);
+    return el.classList.add('selected')
+    
+  }
 }
