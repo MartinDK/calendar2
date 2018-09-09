ae3f2d04c15ae948c685a3d9829d50714b464298
Martin Daniel <mtin722@gmail.com>
2018-09-09 20:10:46 +1000

    Refactored code

--- js/events.js
+++ js/events.js
@@ -3,22 +3,25 @@ function addEvents(selector) {
   const el = document.querySelectorAll(selector);
 
   for (let i = 0; i < el.length; i += 1) {
-    el[i].addEventListener('click', () => {
-      eventHandler(el[i])
-    });
+    el[i].addEventListener('click', () => eventHandler(el[i]));
   };
 }
 
 // Event handler
-let eventHandler = (el) => {
-  let elementState;
-  elementState = el.classList.contains('today') ? todaySelected(el) : el;
-  elementState = el.classList.contains('selected') ? toggleSelection(el) : toggleSelection(el);
-  
-  return elementState;
+let eventContains = (e, el) => el.classList.contains(e);
+
+let eventHandler = el => {
+  let eState;
+  eState = eventContains('today', el) ? togglToday(el) : el;
+  eState = eventContains('selected', el) ? togglSelect(el) : togglSelect(el);
+
+  return eState;
 }
 
 // Events
-
-let todaySelected = el => { return el.classList.contains("selected") ? console.log(`goodbye`) : console.log(`hello`) };
-let toggleSelection = el => { return el.classList.contains("selected") ? el.classList.remove('selected') : el.classList.add('selected') };
+let togglToday = el => {
+  return eventContains("selected", el) ? console.log(`goodbye`) : console.log(`hello`)
+};
+let togglSelect = el => {
+  return eventContains("selected", el) ? el.classList.remove('selected') : el.classList.add('selected')
+};
