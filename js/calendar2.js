e6bced3580c1b7a78daec3a4fcc8fe1b09f80cda
Martin D K <mtin727@gmail.com>
2018-09-10 17:14:23 +1000

    monthArray is populated with objects

--- js/calendar2.js
+++ js/calendar2.js
@@ -3,15 +3,18 @@
 class Calendar2 {
   constructor() {
 
-    this.today = new Date();
     // labels for the days of the week
     this.daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     // months array in order
     this.monthsName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     // the days of the week for each month, in order
     this.daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-    this.month = [];
+    
+    this.today = new Date();
+    this.date = this.today.getDate();
+    this.month = this.today.getMonth();
+    this.year = this.today.getFullYear();
+    this.monthArray = [];
   }
   static createCalendar(id) { 
 
@@ -22,23 +25,28 @@ class Calendar2 {
     calendar2.highlightToday();
   }
   initMonthArray(month) {
+    // initial values
     const monthLength = this.daysInMonth[month];
+    const firstOfMonth = new Date(this.year, this.month, 1);
+    // build month array
     let monthArray = Array.apply(null, { length: monthLength }).map(Number.call, Number);
-    this.month = monthArray.map(x => x + 1); // Adjust first day 0=>1
+    this.monthArray = monthArray.map(x => x = {day: x+1}); // Adjust first day 0=>1
+    this.monthArray[0].firstOfMonth = firstOfMonth.getDay();
   }
   createMonthHTML() {
-
+    // Intialise month array
     this.initMonthArray(this.today.getMonth());
-    const startWeekday = this.getToday().firstDay;
+
+    const startWeekday = this.monthArray[0].firstOfMonth;
     let html = `<table class="calendar2 calendar-table"> <tbody ><tr><th colspan="10" > ${this.getToday().day} &nbsp;${this.getToday().date}<sup>th</sup>&nbsp;${this.getToday().month}&nbsp;${this.getToday().year}</th ></tr>`;
     html += '<tr class="calendar-header"><td class="calendar-header-day">Mon</td><td class="calendar-header-day">Tue</td><td class="calendar-header-day">Wed</td><td class="calendar-header-day">Thu</td><td class="calendar-header-day">Fri</td><td class="calendar-header-day">Sat</td><td class="calendar-header-day">Sun</td></tr><tr>';
     
     const emptyCell = startWeekday;
-    for (let i = 0; i < emptyCell; i += 1) {
+    for (let i = 1; i < emptyCell; i += 1) {
       html += '<td></td>';
     }
-    for (let i = 0; i < this.month.length; i += 1) {
-      const day = this.month[i];
+    for (let i = 0; i < this.monthArray.length; i += 1) {
+      const day = this.monthArray[i].day;
       const cell = emptyCell + day;
       if (cell === 1) {
         html += `<td class="calendar-day" id="id-${day}">${day}</td>`;
@@ -56,7 +64,7 @@ class Calendar2 {
     document.getElementById(id).insertAdjacentHTML('beforeend', html);
   }
   highlightToday() {
-    const e = document.querySelectorAll(`#id-${this.getToday().date}`);
+    const e = document.querySelectorAll(`#id-${this.date}`);
     e[0].classList.add('today');
   }
   dateOrdinal() {
